f2b31fa0bc1d92beaff933135b3ec0df17051715
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-08-12 23:16:41 +0300

    task20-1

--- Домашняя работа 3/task20/Program.cs
+++ Домашняя работа 3/task20/Program.cs
@@ -3,3 +3,18 @@
 // в 3D пространстве.
 // A (3,6,8); B (2,1,-7), -> 15.84
 // A (7,-5, 0); B (1,-1,9) -> 11.53
+
+Console.WriteLine("Введите координату X1");
+double X1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y1");
+double Y1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Z1");
+double Z1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату X2");
+double X2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Y2");
+double Y2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите координату Z2");
+double Z2 = Convert.ToDouble(Console.ReadLine());
+double dist = X2 - X1 * 2 + Y2 - Y1 * 2 + (Z2 - Z1) * 2;
+Console.WriteLine(dist);
