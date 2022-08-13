7772922b0a51b5f792f15f13776a5667bed7a91d
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-08-13 15:38:54 +0300

    task20

--- Домашняя работа 3/task20/Program.cs
+++ Домашняя работа 3/task20/Program.cs
@@ -16,5 +16,5 @@ Console.WriteLine("Введите координату Y2");
 double Y2 = Convert.ToDouble(Console.ReadLine());
 Console.WriteLine("Введите координату Z2");
 double Z2 = Convert.ToDouble(Console.ReadLine());
-double dist = X2 - X1 * 2 + Y2 - Y1 * 2 + (Z2 - Z1) * 2;
-Console.WriteLine(dist);
+double dist = Math.Sqrt(Math.Pow(X2 - X1, 2) + Math.Pow(Y2 - Y1, 2) + Math.Pow(Z2 - Z1, 2));
+Console.Write($"Расстояние между точками: {dist:f2}");
