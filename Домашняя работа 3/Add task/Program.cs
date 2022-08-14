68dc4978577aa5101b17a4df20cb1b502283bf2c
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-08-14 19:39:56 +0300

    Ex019

---
+++ Домашняя работа 3/Add task/Program.cs
@@ -0,0 +1,20 @@
+﻿// void PrintArray(int[] array)
+// {
+//     int count = array.Length;
+//     for (int i = 0; i < count; i++)
+//     {
+//         Console.Write($"{array[i]} ");
+//     }
+//     Console.WriteLine();
+// }
+
+// void Sort(int[] arr)
+// {
+//     for (int i = 0; i < (arr.Length/2); i++)
+//     {
+//         int temporary = arr[i];
+//         arr[i] = arr[arr.Length - i - 1];
+//         arr[arr.Length - i - 1] = temporary;
+//     }
+// }
+
--- Домашняя работа 3/task19/Program.cs
+++ Домашняя работа 3/task19/Program.cs
@@ -3,3 +3,22 @@
 // 12821 -> да
 // 23432 -> да
 
+Console.WriteLine("Введите пятизначное число");
+string array = Console.ReadLine();
+string[] words = array.Split();
+int size = array.Length;
+if (size == 5)
+{
+    if(array[0]==array[4] & array[1]==array[3])
+    {
+        Console.WriteLine("Да");
+    }
+    else
+    {
+        Console.WriteLine("Нет");
+    }
+}
+else
+{
+    Console.WriteLine("неверно введено число");
+}
