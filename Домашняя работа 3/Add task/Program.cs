ccc4a6da6ead8a7317f21a05f0b7b422a3256fa5
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-08-14 22:16:56 +0300

    add task(2)

--- Домашняя работа 3/Add task/Program.cs
+++ Домашняя работа 3/Add task/Program.cs
@@ -1,20 +1,39 @@
-﻿// void PrintArray(int[] array)
-// {
-//     int count = array.Length;
-//     for (int i = 0; i < count; i++)
-//     {
-//         Console.Write($"{array[i]} ");
-//     }
-//     Console.WriteLine();
-// }
+﻿// Доп. задача
+//Написать программу для разворота массива
 
-// void Sort(int[] arr)
-// {
-//     for (int i = 0; i < (arr.Length/2); i++)
-//     {
-//         int temporary = arr[i];
-//         arr[i] = arr[arr.Length - i - 1];
-//         arr[arr.Length - i - 1] = temporary;
-//     }
-// }
+void FillArray(int[] numbers)
+{
+    int Length = numbers.Length;
+    int index = 0;
+    while (index < Length)
+    {
+        numbers[index] = new Random().Next(1, 9);
+        index++;
+    }
 
+}
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    for (int i = 0; i < count; i++)
+    {
+        Console.Write($"{array[i]} ");
+    }
+    Console.WriteLine();
+}
+
+void Sort(int[] array)
+{
+    for (int i = 0; i < (array.Length / 2); i++)
+    {
+        int temporary = array[i];
+        array[i] = array[array.Length - i - 1];
+        array[array.Length - i - 1] = temporary;
+    }
+}
+
+int[] array = new int[9];
+FillArray(array);
+PrintArray(array);
+Sort(array);
+PrintArray(array);
