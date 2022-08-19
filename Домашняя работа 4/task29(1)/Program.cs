b0c6c8f458dc7bbe5178169cbd4fdebaf287abb4
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-08-19 11:01:56 +0300

    task29(1)

---
+++ Домашняя работа 4/task29(1)/Program.cs
@@ -0,0 +1,8 @@
+﻿int[] Array = new int[8];
+Console.WriteLine("Введите восемь целых чисел для создания массива:");
+for (int i = 0; i < Array.Length; i++)
+{
+    Array[i] = Convert.ToInt32(Console.ReadLine());
+}
+Console.WriteLine("Введенный массив:");
+Console.Write(string.Join(",", Array));
