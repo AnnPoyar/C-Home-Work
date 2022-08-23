44bd3688dbdc46d9558e75cc0d9460a07a512fec
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-08-23 17:08:15 +0300

    ex038(1)

--- Домашняя работа 5/task38(1)/Program.cs
+++ Домашняя работа 5/task38(1)/Program.cs
@@ -3,21 +3,22 @@
 
 // [3 7 22 2 78] -> 76
 
-double[] array = new double[6];
+int[] array = new int[6];
 Random rand = new Random();
-double max = array[0];
-double min = array[0];
-double res = 0;
+int max = array[0];
 for (int i = 0; i < array.Length; i++)
 {
-    array[i] = rand.Next(-10, 101) * 0.1;
+    array[i] = rand.Next(1, 101);
     if (max < array[i]) max = array[i];
-    if (array[i] < min) min = array[i];
-
 }
 Console.WriteLine(string.Join(", ", array));
-Console.WriteLine();
+int min = array[0];
+for (int i = 0; i < array.Length; i++)
+{
+    if (array[i] < min) min = array[i];
+}
+
 Console.WriteLine($"Максимальное число в массиве = {max}");
 Console.WriteLine($"Минимальное число в массиве = {min}");
-res = max - min;
+int res = max - min;
 Console.WriteLine($"Разница между максимальным и минимальным элементами массива = {res}");
