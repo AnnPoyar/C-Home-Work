9d0217ecdd4bf97a8bfbb84376016b782fbff845
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-08-24 16:01:26 +0300

    task 41,43

--- Домашняя работа 6/task41/Program.cs
+++ Домашняя работа 6/task41/Program.cs
@@ -2,4 +2,17 @@
 
 // 0, 7, 8, -2, -2 -> 2
 
-// 1, -7, 567, 89, 223-> 3
+// 1, -7, 567, 89, 223-> 4
+Console.WriteLine("Введите количество чисел для выборки");
+int M = Convert.ToInt32(Console.ReadLine());
+int[] arr = new int[M];
+Console.WriteLine("Введите числа для дальнейшей выборки");
+int count = 0;
+
+for (int i = 0; i < arr.Length; i++)  
+{
+    arr[i] = Convert.ToInt32(Console.ReadLine());
+    if(arr[i] > 0) count++; 
+}
+
+Console.WriteLine($"Количество чисел больше 0: {count}");
--- Домашняя работа 6/task43/Program.cs
+++ Домашняя работа 6/task43/Program.cs
@@ -3,4 +3,20 @@
 
 // b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
 
+Console.WriteLine("Введите значение коэффициента b1");
+double b1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение коэффициента k1");
+double k1 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение коэффициента b2");
+double b2 = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите значение коэффициента k2");
+double k2 = Convert.ToDouble(Console.ReadLine());
+double x = (b2 - b1)/(k1 - k2);
+double y1 = k1 * x + b1;
+double y2 = k2 * x + b2;
+if(y1 == y2) Console.WriteLine($"Координата точки пересечения ({x};{y1})");
+if(y1 != y2) Console.WriteLine($"Прямые не имеют точки пересечения. Прямые параллельны.");
+ 
+
+
 
