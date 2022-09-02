2415c5466abe5cb36a53cfb9bf22e40ea9b638cc
AnnaPoyarkova <lazko.anna92@gmail.com>
2022-09-02 23:08:22 +0300

    task 64-68

---
+++ Домашняя работа 9/task066/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 66: Задайте значения M и N. Напишите программу, которая найдёт 
+// сумму натуральных элементов в промежутке от M до N.
+
+// M = 1; N = 15 -> 120
+// M = 4; N = 8. -> 30
+
+Console.WriteLine("Введите M");
+int number = Convert.ToInt32(Console.ReadLine());
+Console.WriteLine("Введите N");
+int numb = Convert.ToInt32(Console.ReadLine());
+
+
+int SumRec(int m, int n)
+{
+    if(n<=m) return m;
+    else return n + SumRec(m, n-1);
+}
+
+Console.WriteLine(SumRec(number, numb));
+
---
+++ Домашняя работа 9/task068/Program.cs
@@ -0,0 +1,22 @@
+﻿// Задача 68: Напишите программу вычисления функции Аккермана с помощью рекурсии. 
+// Даны два неотрицательных числа m и n.
+
+// m = 2, n = 3 -> A(m,n) = 9
+
+Console.WriteLine("Введите неотрицательное число m");
+double number = Convert.ToDouble(Console.ReadLine());
+Console.WriteLine("Введите неотрицательное число n");
+double numb = Convert.ToDouble(Console.ReadLine());
+if (number >= 0 && numb >= 0)
+{
+    Console.Write(AkkermansFunction(number, numb));
+}
+
+double AkkermansFunction(double m, double n)
+{
+    if(m==0) return n+1;
+    if( n==0) return AkkermansFunction(m-1, 1);
+    else return AkkermansFunction( m-1, AkkermansFunction(m, n-1));
+}
+
+
